74c70b2fd277b28a61b8302e828e14f0cfbf1eca
Sofi <vessona2000@gmail.com>
2018-07-01 13:11:02 -0700

    ADDED FRAGMENTS

--- Group3_Skytrain_Project/app/src/main/java/com/yamibo/bbs/group3_skytrain_project/activities/NearbyActivity.java
+++ Group3_Skytrain_Project/app/src/main/java/com/yamibo/bbs/group3_skytrain_project/activities/NearbyActivity.java
@@ -1,5 +1,6 @@
 package com.yamibo.bbs.group3_skytrain_project.activities;
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -7,9 +8,11 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.yamibo.bbs.group3_skytrain_project.R;
 import com.yamibo.bbs.group3_skytrain_project.adapter.CardAdapter;
+import com.yamibo.bbs.group3_skytrain_project.map.MapFragment;
 import com.yamibo.bbs.group3_skytrain_project.models.Stop;
 import com.yamibo.bbs.group3_skytrain_project.service.TransLinkService;
 
@@ -27,45 +30,67 @@ public class NearbyActivity extends AppCompatActivity {
     private List<Stop> data;
     private RecyclerView mRecyclerView;
     private CardAdapter mCardAdapter;
+    private boolean mTwoPane = false;
+    Location mCurrentLocation;
+    private long UPDATE_INTERVAL = 60000;  /* 60 secs */
+    private long FASTEST_INTERVAL = 5000; /* 5 secs */
 
+    private final static String KEY_LOCATION = "location";
+
+    /*
+     * Define a request code to send to Google Play services This code is
+     * returned in Activity.onActivityResult
+     */
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nearby);
 
+        loadNearbyStops();
+
+
+     /*    if(findViewById(R.id.stop_map_container) != null){
+            mTwoPane = true;
+
+
+           MapFragment mapFragment = MapFragment.newInstance();
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.stop_map_container, mapFragment)
+                    .addToBackStack(null)
+                    .commit();
+        }
+*/
+
+    }
+
+
+
+    public void loadNearbyStops(){
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://api.translink.ca/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-        btn_test = findViewById(R.id.btn_test);
-        btn_test.setOnClickListener(new View.OnClickListener() {
+        TransLinkService service = retrofit.create(TransLinkService.class);
+        //TODO: Add dynemic upload of current location
+        Call<List<Stop>> call = service.getStop("fH8nhLCTC142J3YXmtLC",49.226258,-123.000436);
+        call.enqueue(new Callback<List<Stop>>() {
             @Override
-            public void onClick(View v) {
+            public void onResponse(Call<List<Stop>> call, Response<List<Stop>> response) {
+                data = response.body();
+                mCardAdapter = new CardAdapter(data);
+                mRecyclerView.setAdapter(mCardAdapter);
+                Log.d("OnResponse", "hi");
+            }
 
-                Retrofit retrofit = new Retrofit.Builder()
-                        .baseUrl("http://api.translink.ca/")
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build();
-
-                TransLinkService service = retrofit.create(TransLinkService.class);
-                //TODO: Add dynemic upload of current location
-                Call<List<Stop>> call = service.getStop("fH8nhLCTC142J3YXmtLC",49.226258,-123.000436);
-                call.enqueue(new Callback<List<Stop>>() {
-                    @Override
-                    public void onResponse(Call<List<Stop>> call, Response<List<Stop>> response) {
-                        data = response.body();
-                        mCardAdapter = new CardAdapter(data);
-                        mRecyclerView.setAdapter(mCardAdapter);
-                        Log.d("OnResponse", "hi");
-                    }
-
-                    @Override
-                    public void onFailure(Call<List<Stop>> call, Throwable t) {
-                        Log.d("Error",t.getMessage());
-                    }
-                });
-
+            @Override
+            public void onFailure(Call<List<Stop>> call, Throwable t) {
+                Log.d("Error",t.getMessage());
             }
         });
     }
--- Group3_Skytrain_Project/app/src/main/java/com/yamibo/bbs/group3_skytrain_project/adapter/CardAdapter.java
+++ Group3_Skytrain_Project/app/src/main/java/com/yamibo/bbs/group3_skytrain_project/adapter/CardAdapter.java
@@ -1,13 +1,20 @@
 package com.yamibo.bbs.group3_skytrain_project.adapter;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.yamibo.bbs.group3_skytrain_project.R;
+import com.yamibo.bbs.group3_skytrain_project.activities.StopDetailActivity;
+import com.yamibo.bbs.group3_skytrain_project.map.MapFragment;
 import com.yamibo.bbs.group3_skytrain_project.models.Stop;
 
 import java.util.ArrayList;
@@ -15,6 +22,7 @@ import java.util.List;
 
 public class CardAdapter extends RecyclerView.Adapter<CardAdapter.ViewHolder> {
     List<Stop> mItems = new ArrayList<>();
+
     public CardAdapter(List<Stop> mItems) {
         this.mItems = mItems;
     }
@@ -38,12 +46,25 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.ViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CardAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull CardAdapter.ViewHolder holder, final int position) {
         Stop stop = mItems.get(position);
         String stopName = stop.getOnStreet()+" FS " + stop.getOnStreet();
         holder.stop_name.setText(stopName);
         holder.dist.setText(""+stop.getDistance());
         holder.routes.setText(stop.getRoutes());
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = v.getContext();
+                Intent intent = new Intent(context, StopDetailActivity.class);
+                intent.putExtra("stopNo", mItems.get(position).getStopNo());
+                context.startActivity(intent);
+            }
+        });
+        //TODO: add mTwoPane code to add marker on map
+
+
     }
 
     @Override
@@ -51,12 +72,14 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.ViewHolder> {
         return mItems.size();
     }
     class ViewHolder extends RecyclerView.ViewHolder {
+        final View mView;
         public TextView stop_name;
         public TextView dist;
         public TextView routes;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            mView = itemView;
             stop_name = (TextView) itemView.findViewById(R.id.stop_name);
             dist = (TextView) itemView.findViewById(R.id.dist);
             routes = (TextView) itemView.findViewById(R.id.routes);
